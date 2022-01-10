19896de1d6270e16467ddc1f27ac8cd43d92211f
Kingstalux <kingsleyngu25@gmail.com>
2022-01-10 15:13:24 +0100

    Add code

---
+++ animals.rb
@@ -0,0 +1,35 @@
+class Animal
+    def initialize(type, number_of_legs, name = "Unknown")
+      @id = Random.rand(1..1000)
+      @name = name
+      @number_of_legs = number_of_legs
+      @type = type
+    end
+    def id
+        @id
+      end
+    
+      def type
+        @type
+      end
+    
+      def number_of_legs
+        @number_of_legs
+      end
+    
+      def name
+        @name
+      end
+    
+      def name=(value)
+        @name = value
+      end
+    #   attr_reader :name
+    #   attr_writer :name
+    #   attr_accessor :name
+  
+      def speak
+        "grrrr"
+      end
+      
+  end
